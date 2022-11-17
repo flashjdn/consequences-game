bf240c7035882b0be329ac0bae93f8e7efbd0d7f
fiona <fiona.kitchen@yahoo.co.uk>
2022-11-17 19:19:16 +0000

    added stuff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-webSocket = new WebSocket("ws://51.141.52.52:1234")
+const webSocket = new WebSocket("ws://51.141.52.52:1234")
+
+// Connection opened
+webSocket.addEventListener('open', (event) => {
+  webSocket.send('Hello Server!');
+});
+
+webSocket.onmessage = (event) => {
+  console.log(event.data);
+}
+
+
